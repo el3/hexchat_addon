ad5050e18f5b74dae214d8dbe9f5000084f332ce
el3 <bogisivertsen@gmail.com>
2015-12-18 01:23:55 +0100

    Create plugin.py

---
+++ plugin.py
@@ -0,0 +1,26 @@
+__module_name__ = "hexchat_translator"
+__module_version__ = "0.1"
+__module_description__ = "Python module to translate messages in hexchat"
+__author__ = "el3"
+
+YOUR_PYTHON = "/home/user/anaconda2/bin/python2.7" #example if you got anaconda
+
+import hexchat
+import sys,os
+print sys.version
+from subprocess import Popen,PIPE
+
+print os.getcwd()
+
+def echo(word, word_eol, userdata):
+    try:
+        p1 = Popen([YOUR_PYTHON, "translate.py", word_eol[3][1:]], stdout=PIPE,stderr=PIPE)
+        res = p1.communicate()[0]
+        nick = word[0].split("!")[0].replace(":","")
+        if len(res) > 0:
+            print('\035', nick,"said:",res.replace("\n",""))
+        return hexchat.EAT_NONE
+    except:
+        return hexchat.EAT_NONE
+
+hexchat.hook_server("PRIVMSG", echo)

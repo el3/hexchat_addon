94189b43cd95ff0d64fc62ab1fdeb52d045b4d77
el3 <bogisivertsen@gmail.com>
2015-12-18 15:04:32 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,22 +1,22 @@
 __module_name__ = "hexchat_translator"
-__module_version__ = "0.1"
+__module_version__ = "0.2"
 __module_description__ = "Python module to translate messages in hexchat"
 __author__ = "el3"
-
-YOUR_PYTHON = "/home/user/anaconda2/bin/python2.7" #example if you got anaconda
 
 import hexchat
 from textblob import TextBlob
 
+print "Hexchat Translator loaded"
+
 def echo(word, word_eol, userdata):
     try:
-        original = TextBlob(word_eol[3][1:])
+        original = TextBlob(word_eol[3][1:].decode("utf-8"))
         lang = original.detect_language()
         nick = word[0].split("!")[0].replace(":","")
         if lang != "en":
-            res = original.translate(from_lang=lang,to='en'),"(From lang=%s)" % lang
+            res = original.translate(from_lang=lang,to='en')
         if len(res) > 0:
-            print('\037\00304 '+ nick+" said: "+res.replace("\n",""))
+            print('\037\00304 ' + nick + " said: " + str(res).replace("\n","") + " (From lang=%s)" % str(lang))
         return hexchat.EAT_NONE
     except:
         return hexchat.EAT_NONE

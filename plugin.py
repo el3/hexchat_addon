14dc5a2a6a9ec9de60d6cb6ccff53869bd92be39
el3 <bogisivertsen@gmail.com>
2015-12-18 20:52:51 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,32 +1,46 @@
 __module_name__ = "hexchat_translator"
 __module_version__ = "0.2"
 __module_description__ = "Python module to translate messages in hexchat"
-__author__ = "el3"
+__author__ = "el3 and aq2"
 
-import hexchat
+try:
+    import hexchat
+except ImportError:
+    exit("This program is a HexChat plugin, not a standalone program.")
 from textblob import TextBlob
 import threading
+
+# Change this to set a different language
+my_language = "en"
+
 
 threads = []
 
 def echo(word, word_eol, userdata):
+    global my_language
     try:
         original = TextBlob(word_eol[3][1:].decode("utf-8"))
         lang = original.detect_language()
         nick = word[0].split("!")[0].replace(":","")
-        if lang != "en":
-            res = original.translate(from_lang=lang,to='en')
+        if lang != my_language:
+            res = original.translate(from_lang=lang, to=my_language)
         if len(res) > 0:
-            print('\037\00304' + nick + " said: " + str(res).replace("\n","") + " (From lang=%s)" % str(lang))
+            print("\037\00312" + nick + " said: " + str(res).replace( \
+                  "\n","") + " (From lang=%s)" % str(lang))
         return hexchat.EAT_NONE
     except:
         return hexchat.EAT_NONE
 
 def echothread(word, word_eol, userdata):
+    global threads
     threads.append(threading.Thread())
     threads[-1].run = lambda: echo(word, word_eol, userdata)
     threads[-1].start()
 
-print "Hexchat Translator loaded"
+def unload_cb(userdata):
+    print("Hexchat Translator unloaded")
 
 hexchat.hook_server("PRIVMSG", echothread)
+hexchat.hook_unload(unload_cb)
+
+print("Hexchat Translator loaded")

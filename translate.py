d26ccd30ae0c4d0f2db7060cfcb182e3be89d6d7
el3 <bogisivertsen@gmail.com>
2015-12-18 02:06:16 +0100

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,8 +1,10 @@
 import sys
 from textblob import TextBlob
 
+recieve = sys.argv[1].decode('utf8')
 
-original = TextBlob(unicode(sys.argv[1]))
+original = TextBlob(recieve)
+
 lang = original.detect_language()
 if lang != "en":
      print original.translate(from_lang=lang,to='en'),"(From lang=%s)" % lang
